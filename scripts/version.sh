f04ef061b42108096d6b6d52519e0a5d33ce00cc
Evhenii Olenovych <eolenovych@stanfy.com.ua>
2012-03-01 20:13:17 +0000

    change

--- scripts/version.sh
+++ scripts/version.sh
@@ -5,22 +5,24 @@ i=0
 
 list=$(find ../../.. -iname "*-Info.plist" | egrep -iv "Libs|cintegration|Frameworks|test")
 
-if [ -n "${list}" ]; then
-
-while read line
-do
-
-if [ $i -eq 1 ]
+if [ -n "${list}" ]
 then
-  ver=$(echo "$line" | sed 's/\<string\>//g'| sed 's/\<\/string\>//g')
-  echo "$ver"
-  i=0
-  break
-fi
 
-if echo "$line" | grep -i "$str" >/dev/null; then
-  i=1
-fi
+ while read line
+	do
 
-done <  ${list}
-fi
+	if [ $i -eq 1 ]
+		then
+			ver=$(echo "$line" | sed 's/\<string\>//g'| sed 's/\<\/string\>//g')
+			echo "$ver"
+			i=0
+			break
+	fi
+
+	if echo "$line" | grep -i "$str" >/dev/null; then
+		i=1
+	fi
+
+	done <  "${list}"
+	fi
+	
