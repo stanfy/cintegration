ff296a4c5c3f6b81da9d516d642c40606367e631
Evhenii Olenovych <eolenovych@stanfy.com.ua>
2012-03-01 18:18:55 +0000

    version.sh change

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,1 +1,26 @@
-#!/bin/shi=0plist_ver=$(find ../../.. -name *-Info.plist)while read line do  if [ $i -eq 1 ]  then    echo $line | sed 's/<string>//' | sed 's/<\/string>//'    break  fi  if [ `echo $line | grep "CFBundleVersion"` ]  then   i=1  fi done < $plist_ver
+#!/bin/sh
+
+#!/bin/sh
+
+str="CFBundleVersion"
+i=0
+
+list=$(find ../../.. -iname "*-Info.plist" | egrep -iv "Libs|cintegration|Frameworks|test")
+
+
+while read line
+do
+
+if [ $i -eq 1 ]
+then
+  ver=$(echo "$line" | sed 's/\<string\>//g'| sed 's/\<\/string\>//g')
+  echo "$ver"
+  i=0
+  break
+fi
+
+if echo "$line" | grep -i "$str" >/dev/null; then
+  i=1
+fi
+
+done <  ${list}
