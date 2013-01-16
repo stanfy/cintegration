3e5bc9c717306e438f5c68a841258c39037af249
Evhenii Olenovych <eolenovych@stanfy.com.ua>
2013-01-16 18:34:22 +0200

    add pods.sh

---
+++ scripts/pods.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+WAIT_TIME=60
+
+podfile=$(find . -name Podfile -maxdepth 2 )
+
+podfile_dir=$(echo "$podfile" | xargs dirname)
+
+mod_time=$(stat -f %m "$podfile")
+
+now_time=$(date +%s)
+
+let "diff_time = $now_time - $mod_time"
+
+if [ $diff_time -lt $WAIT_TIME ]
+then
+	echo
+        echo "-- PODS INSTALLING --"
+        cd "$podfile_dir"
+        pod install --no-color
+        echo
+fi
+                                        
