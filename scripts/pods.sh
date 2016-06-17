a460f6f1b0dc9657819f35688ec955940dfa0c10
Evhenii Olenovych <eolenovych@stanfy.com.ua>
2016-06-17 13:39:30 +0300

    add POD_VERSION env

--- scripts/pods.sh
+++ scripts/pods.sh
@@ -4,33 +4,6 @@ WAIT_TIME=60
 
 podfile=$(find . -name Podfile -maxdepth 2 )
 
-#gemfile=$(find . -name Gemfile -maxdepth 2 )
-
-
-#### INSTALL GEMS
-#if [ -n "$gemfile" ]
-#then
-#  gemfile_dir=$(echo "gemfile" | xargs dirname)
-#
-#  mod_time=$(stat -f %m "$gemfile")
-#
-#  now_time=$(date +%s)
-#
-#  let "diff_time = $now_time - $mod_time"
-#
-#  if [ $diff_time -lt $WAIT_TIME ]
-#  then
-#  echo
-#        echo "-- PODS INSTALLING via GEMS--"
-#        cd "$podfile_dir"
-#        bundle install
-#        bundle exec pod install --no-color
-#        echo
-#  fi
-#  exit 0
-#fi
-
-
 ##### INSTALL PODS
 if [ -n "$podfile" ]
 then
@@ -46,10 +19,16 @@ then
   then
   	echo
         echo "-- PODS INSTALLING --"
-                            
-        pod_version=$(pod --version | tail -n1)
+
+	podSpecifiedVersion=''
+	if [ -n "$POD_VERSION" ]
+	then
+		podSpecifiedVersion="_${POD_VERSION}_"
+	fi
+	                            
+        pod_version=$(pod $podSpecifiedVersion --version | tail -n1)
         echo "[INFO] Using pod version: $pod_version"
-        LANG=en_US.UTF-8 pod install --no-ansi
+        LANG=en_US.UTF-8 pod $podSpecifiedVersion install --no-ansi
                                                     
   fi
 fi
